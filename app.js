6649cc8a84d0fe96cd81c91d17333c63be2b5164
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-04-29 10:54:34 +0200

    Coding the javascript part of the game

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let userScore = 0;
 let computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
+const userScore = document.getElementById("user-score");
+const computerScore = document.getElementById("computer-score");
+const scoreBoard = document.querySelector(".score-board");
