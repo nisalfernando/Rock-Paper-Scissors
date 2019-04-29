5f497ab169a6cd1ad945f175da6ae4e6fce9d2a9
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-04-29 10:58:13 +0200

    Selecting elements

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ let computerScore = 0;
 const userScore = document.getElementById("user-score");
 const computerScore = document.getElementById("computer-score");
 const scoreBoard = document.querySelector(".score-board");
+const result = document.querySelector(".result > p");
+const rock = document.getElementById("r");
