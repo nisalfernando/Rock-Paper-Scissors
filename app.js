2bcfcc91a38764a06dddbdfd147538bb73665c25
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-05-28 12:52:09 +0200

    Fixing bugs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const paper = document.getElementById("p");
 const scissors = document.getElementById("s");
 
 function getComputerChoice() {
-  const choice = ["r", "p", "s"];
+  const choices = ["r", "p", "s"];
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 }
